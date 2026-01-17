efe0c42b8f5cbca0968391f8fd3d8649e0694dc4
Angel Kürten <angelkurten@hotmail.com>
2026-01-17 11:45:29 -0500

    fix: prácticas agrícolas -> prácticas agronómicas

--- components/Services.tsx
+++ components/Services.tsx
@@ -25,7 +25,7 @@ const activities: Activity[] = [
   },
   {
     title: 'Asistencia Técnica',
-    description: 'Acompañamiento especializado en sanidad vegetal, fertilización y buenas prácticas agrícolas.',
+    description: 'Acompañamiento especializado en sanidad vegetal, fertilización y buenas prácticas agronómicas.',
     icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2'
   },
   {
